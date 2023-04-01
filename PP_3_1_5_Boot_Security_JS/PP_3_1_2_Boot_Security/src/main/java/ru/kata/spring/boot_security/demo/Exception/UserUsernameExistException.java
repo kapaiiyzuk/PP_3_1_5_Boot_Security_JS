ab7b028bb8af1a9909a5c7da4594e3f5fdbc2718
kapaiiyzuk <118076489+kapaiiyzuk@users.noreply.github.com>
2023-04-02 00:14:33 +0300

    Delete PP_3_1_5_Boot_Security_JS directory

--- PP_3_1_5_Boot_Security_JS/PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/Exception/UserUsernameExistException.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.Exception;
-
-import org.springframework.dao.DataIntegrityViolationException;
-
-public class UserUsernameExistException extends DataIntegrityViolationException {
-    public UserUsernameExistException(String msg) {
-        super(msg);
-    }
-}
--- PP_3_1_5_Boot_Security_JS/PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.repository;
-
-import ru.kata.spring.boot_security.demo.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
